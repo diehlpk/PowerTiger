7cebb716c50a12e5b0a157798705b106bc74968b
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2019-02-15 17:22:13 +0100

    Updated Vc build

--- build-Vc.sh
+++ build-Vc.sh
@@ -11,15 +11,16 @@ fi
 cd $SOURCE_ROOT
 if [ ! -d "Vc/" ]; then
     git clone https://github.com/VcDevel/Vc.git
+    cd Vc
+    git submodule update --init
+    cd ..
 fi
 
 cd Vc
-git fetch --all --tags --prune
-git checkout tags/1.4.1
 cd $INSTALL_ROOT
 mkdir -p Vc
 cd Vc
 mkdir -p build
 cd build
-$INSTALL_ROOT/cmake/bin/cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT/Vc -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF $SOURCE_ROOT/Vc
+$INSTALL_ROOT/cmake/bin/cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT/Vc -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCHITECTURE=Scalar -DBUILD_TESTING=OFF -DVC_IMPL=VC_IMPL_Scalar $SOURCE_ROOT/Vc
 make -j${PARALLEL_BUILD} VERBOSE=1 install
